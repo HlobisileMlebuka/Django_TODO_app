1199187b4054118cb0f1bc769056357c9af6c35f
HlobisileMlebuka <mlebukah@gmail.com>
2019-09-20 16:06:35 +0200

    .

--- myproject/todo/urls.py
+++ myproject/todo/urls.py
@@ -4,5 +4,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add', views.add_todo, name='add'),
+    path('complete/<todo_id>', views.complete_todo, name='complete'),
+    path('deletecomplete/', views.delete_completed, name='deletecomplete'),
+    path('deleteall', views.delete_all, name='deleteall')
+
 ]
--- myproject/todo/views.py
+++ myproject/todo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ToDo
 from .forms import ToDoForm
+from django.views.decorators.http import require_POST
 
 def index(request):
     todo_list = ToDo.objects.order_by('id')
@@ -9,3 +10,37 @@ def index(request):
 
     context = {'todo_list' : todo_list, 'form': form} 
     return render(request,'todo/index.html', context)
+
+@require_POST
+def add_todo(request):
+    '''takes in the form data, that is instantiated with the form data'''
+
+    form = ToDoForm(request.POST)
+    print(request.POST['text'])
+    
+    #save form data to database
+    if form.is_valid():
+        new_todo = ToDo(text=request.POST['text'])
+        new_todo.save()
+    
+    #redirected back to the index
+    return redirect('index')
+
+def complete_todo(request, todo_id):
+
+    todo = ToDo.objects.get(pk=todo_id)
+    todo.complete = True
+    todo.save()
+
+    return redirect('index')
+
+def delete_completed(request):
+    #search database for completed todos
+    ToDo.objects.filter(complete__exact=True).delete()
+
+    return redirect('index')
+
+def delete_all(request):
+    ToDo.objects.all().delete()
+
+    return redirect('index')
